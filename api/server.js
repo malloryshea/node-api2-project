135392e74658b0129f47497bbe9900720f305ea7
Mallory Shea <malloryshea@Mallorys-MacBook-Air.local>
2022-01-13 10:49:37 -0600

    endpoints

--- api/server.js
+++ api/server.js
@@ -1,2 +1,10 @@
 // implement your server here
 // require your posts router and connect it here
+const express = require('express')
+const server = express()
+server.use(express.json())
+const router = require("./posts/posts-router")
+
+server.use("/api/posts", router)
+
+module.exports = server
